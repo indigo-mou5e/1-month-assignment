bcb761a4eb5e799f201bcce829430de623a2861b
indigo-mou5e <himajinn59@gmail.com>
2016-12-09 13:13:30 +0900

    Initial commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+100.times do|i|
+	if i%3==0
+		if i%5==0
+			puts "fizz buzz"
+		else
+			puts "fizz"
+		end
+	else
+		if i%5==0
+			puts "buzz" 
+		else
+			puts i
+	end
+end
+end
---
+++ nabeatsu.rb
@@ -0,0 +1,13 @@
+100.times do|i|
+	if i%3 == 0
+		print(i , "!!!!!!","\n")
+	else if i%10 == 3
+		print(i , "!!!!!!","\n")
+	else if i/10 == 3
+		print(i , "!!!!!!","\n")
+	else 
+		puts i
+	end
+	end
+	end
+end
